fb0664eb12ab543599bbea434b86db0849cbd330
elizabeth paige <wingateep@Icloud.com>
2020-03-02 14:23:22 -0500

    set up outlets + actions

--- KidsFly/KidsFly/LoginViewController.swift
+++ KidsFly/KidsFly/LoginViewController.swift
@@ -8,23 +8,28 @@
 
 import UIKit
 
+enum LoginType {
+    case signUp
+    case signIn
+}
+
 class LoginViewController: UIViewController {
 
+      var loginType = LoginType.signUp
+    
+    @IBOutlet weak var usernameTextField: UITextField!
+     @IBOutlet weak var passwordTextField: UITextField!
+     @IBOutlet weak var loginTypeSegmentedControl: UISegmentedControl!
+     @IBOutlet weak var signInButton: UIButton!
+     
     override func viewDidLoad() {
         super.viewDidLoad()
-
         // Do any additional setup after loading the view.
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+  @IBAction func signInButtonTapped(_ sender: UIButton) {
     }
-    */
-
+    
+   @IBAction func signInTypeChanged(_ sender: UISegmentedControl) {
+    }
 }
