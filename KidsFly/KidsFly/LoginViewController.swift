25972f9f27f75cea7395db49b5aa955a9155ffb5
elizabeth paige <wingateep@Icloud.com>
2020-03-02 14:45:15 -0500

    setUp KidsFlyController - Cant do a whole lot til we get the baseURL for the api

--- KidsFly/KidsFly/LoginViewController.swift
+++ KidsFly/KidsFly/LoginViewController.swift
@@ -14,22 +14,66 @@ enum LoginType {
 }
 
 class LoginViewController: UIViewController {
-
+    
+      var kidsFlyController: KidsFlyController!
       var loginType = LoginType.signUp
     
-    @IBOutlet weak var usernameTextField: UITextField!
+     @IBOutlet weak var usernameTextField: UITextField!
      @IBOutlet weak var passwordTextField: UITextField!
      @IBOutlet weak var loginTypeSegmentedControl: UISegmentedControl!
      @IBOutlet weak var signInButton: UIButton!
      
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
     }
     
   @IBAction func signInButtonTapped(_ sender: UIButton) {
+    guard let kidsFlyController = kidsFlyController else { return }
+   
+            if let username = usernameTextField.text,
+                !username.isEmpty,
+                 let password = passwordTextField.text,
+                !password.isEmpty {
+                let user = User(username: username, password: password)
+             
+                if loginType == .signUp {
+                kidsFlyController.signUp(with: user) { (error) in
+                 if let error = error {
+                        NSLog("Error occurred during sign up: \(error)")
+                } else {
+                   DispatchQueue.main.async {
+                 let alertController = UIAlertController(title: "Sign Up Successful", message: "Now please log in.", preferredStyle: .alert)
+                  let alertAction = UIAlertAction(title: "OK", style: .default, handler: nil)
+                    
+                alertController.addAction(alertAction)
+                self.present(alertController, animated: true) {
+                    self.loginType = .signIn
+                    self.loginTypeSegmentedControl.selectedSegmentIndex = 1
+                    self.signInButton.setTitle("Sign In", for: .normal)
+                 }
+              }
+           }
+        }
+             } else {
+                    kidsFlyController.signIn(with: user) { (error) in
+                        if let error = error {
+                     NSLog("Error logging in: \(error)")
+             } else {
+                    DispatchQueue.main.async {
+                    self.dismiss(animated: true, completion: nil)
+                 }
+              }
+           }
+         }
+      }
     }
     
    @IBAction func signInTypeChanged(_ sender: UISegmentedControl) {
+        if sender.selectedSegmentIndex == 0 {
+                signInButton.setTitle("Sign Up", for: .normal)
+                } else {
+                loginType = .signIn
+                signInButton.setTitle("Sign In", for: .normal)
+        }
     }
 }
---
+++ KidsFly/KidsFly/Models/User.swift
@@ -0,0 +1,14 @@
+//
+//  User.swift
+//  KidsFly
+//
+//  Created by Elizabeth Wingate on 3/2/20.
+//  Copyright © 2020 Keri Levesque. All rights reserved.
+//
+
+import Foundation
+
+struct User: Codable {
+    let username: String
+    let password: String
+}
