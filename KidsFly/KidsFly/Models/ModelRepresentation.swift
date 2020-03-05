ea913f66fd7c25bc87ac7026cfa56675c801190d
Keri Levesque <kerianne166@gmail.com>
2020-03-05 13:30:47 -0500

    added core data model files and convenience

---
+++ KidsFly/KidsFly/Models/ModelRepresentation.swift
@@ -0,0 +1,21 @@
+//
+//  ModelRepresentation.swift
+//  KidsFly
+//
+//  Created by Keri Levesque on 3/5/20.
+//  Copyright © 2020 Keri Levesque. All rights reserved.
+//
+
+import Foundation
+
+struct TripRepresentation: Equatable, Codable {
+    var identifier: String?
+    var airport: String?
+    var airline: String?
+    var completedStatus: Bool?
+    var flightNumber: String?
+    var departureTime: Date?
+    var childrenQty: Int?
+    var carryOnQty: Int?
+    var checkedBagQty: Int?
+}
---
+++ KidsFly/KidsFly/Models/Trip+Convenience.swift
@@ -0,0 +1,59 @@
+//
+//  Trip+Convenience.swift
+//  KidsFly
+//
+//  Created by Keri Levesque on 3/5/20.
+//  Copyright © 2020 Keri Levesque. All rights reserved.
+//
+
+import Foundation
+import CoreData
+
+extension Trip {
+    
+    @discardableResult convenience init(identifier: UUID = UUID(),
+                                         airport: String,
+                                         airline: String,
+                                         completedStatus: Bool = false,
+                                         flightNumber: String,
+                                         departureTime: Date,
+                                         childrenQty: Int16,
+                                         carryOnQty: Int16,
+                                         checkedBagQty: Int16,
+                                         context: NSManagedObjectContext = CoreDataStack.shared.mainContext) {
+         
+         self.init(context: context)
+         self.identifier = identifier
+         self.airport = airport
+         self.airline = airline
+         self.completedStatus = completedStatus
+         self.flightNumber = flightNumber
+         self.departureTime = departureTime
+         self.childrenQty = childrenQty
+         self.carryOnQty = carryOnQty
+         self.checkedBagQty = checkedBagQty
+     }
+     
+     @discardableResult convenience init?(tripRepresentation: TripRepresentation, context: NSManagedObjectContext = CoreDataStack.shared.mainContext) {
+         
+         guard let identifierString = tripRepresentation.identifier,
+             let identifier = UUID(uuidString: identifierString),
+             let airport = tripRepresentation.airport,
+             let airline = tripRepresentation.airline,
+             let completedStatus = tripRepresentation.completedStatus,
+             let flightNumber = tripRepresentation.flightNumber,
+             let departureTime = tripRepresentation.departureTime,
+             let childrenQty = tripRepresentation.childrenQty,
+             let carryOnQty = tripRepresentation.carryOnQty,
+             let checkedBagQty = tripRepresentation.checkedBagQty else { return nil }
+         
+         self.init(identifier: identifier, airport: airport, airline: airline, completedStatus: completedStatus, flightNumber: flightNumber, departureTime: departureTime, childrenQty: Int16(childrenQty), carryOnQty: Int16(carryOnQty), checkedBagQty: Int16(checkedBagQty))
+     }
+     
+     var tripRepresentation: TripRepresentation {
+         return TripRepresentation(identifier: identifier?.uuidString, airport: airport, airline: airline, completedStatus: completedStatus, flightNumber: flightNumber, departureTime: departureTime, childrenQty: Int(childrenQty), carryOnQty: Int(carryOnQty), checkedBagQty: Int(checkedBagQty))
+     }
+    
+    
+    
+}
